b9121f839f4e4ae410a98194fdef787d28c14dd8
Mateus Sanfer <mateus.sanfer@gmail.com>
2025-09-15 21:20:45 -0300

    chore(data.js): atualiza links dos projetos no portfólio

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 import code from "./assets/code2.png";
-import image from "./assets/image.png"
+import sgb from "./assets/sgb.png"
 import apocalipticos from "./assets/apocalipticos.png"
 import code3 from "./assets/code3.jpg"
 const projects = [
   {
-    title: "Meu Projeto 1",
-    subtitle: "Html e Javascript",
-    description: "Apocalípticos é um jogo interativo e insano para grupos de amigos (presencial ou remoto), inspirado em jogos como Amigos de Merda, Verdade ou Desafio e Eu Nunca. Apocalípticos é um jogo interativo e insano para grupos de amigos (presencial ou remoto), inspirado em jogos como Amigos de Merda, Verdade ou Desafio e Eu Nunca. ",
-    link: "https://github.com/seuusuario/projeto1",
-    image: image,
+    title: "Sistema de Gerenciamento Bibliotecário",
+    subtitle: "Html, Javascript, Bootstrap e Firebase",
+    description: "O Sistema de Gerenciamento Bibliográfico foi desenvolvido para atender às necessidades da biblioteca SENAI/CETEC de Araguaína, que atualmente enfrenta limitações em suas rotinas devido ao uso de um sistema gratuito sem suporte adequado. O objetivo principal é otimizar processos como empréstimos, devoluções, catalogação e elaboração de relatórios, proporcionando uma experiência mais eficiente e moderna para bibliotecários e usuários finais.",
+    link: "https://sgbd.netlify.app/",
+    image: sgb,
   },
   {
     title: "Apocallípticos",
